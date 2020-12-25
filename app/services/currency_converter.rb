a9c2e499bd2fc4819d0e36cc401fd8b8a5537816
Tim Hemi (Ops) <tim@reinteractive.net>
2020-12-25 11:46:27 +1100

    refactor exchange rates

--- app/services/currency_converter.rb
+++ app/services/currency_converter.rb
@@ -1,24 +1,38 @@
 class CurrencyConverter
   attr_accessor :eow
 
-  def initialize(eow=nil)
+  def initialize(eow=nil, exhange_rate_filename="config/exchange_rates.yml")
     @eow ||= Time.current.production_end_of_week
   end
 
   def perform(from_currency="AU", to_currency="US")
-    exchange_rate = get_exchange_rate(from_currency, to_currency)
-    au_records = Statistic.where(week_ending_at: eow).where(currency: from_currency)
-    au_records.each do |record|
-      record.original_value = "#{record.value}#{record.currency} (rate: #{exchange_rate})"
-      record.value = (record.value * exchange_rate)
-      record.currency = to_currency
-      record.save
+    if (exchange_rate = get_exchange_rate(from_currency, to_currency))
+      au_records = Statistic.where(week_ending_at: eow).where(currency: from_currency)
+      au_records.each do |record|
+        record.original_value = "#{record.value}#{record.currency} (rate: #{exchange_rate})"
+        record.value = (record.value * exchange_rate)
+        record.currency = to_currency
+        record.save
+      end
     end
   end
 
   private
 
   def get_exchange_rate(from_currency, to_currency)
-    ENV.fetch("AU_US_EXCHANGE_RATE").to_f
+    file_path = Rails.root.join("config","exchange_rates.yml")
+    begin
+      data = YAML.load_file(file_path)[:exchange_rates]
+      data[from_currency.to_sym][to_currency.to_sym][:rate].to_f
+    rescue => e
+      puts "======================"
+      puts "Exhange rate NOT FOUND - no records updated!"
+      puts "----------------------"
+      puts e.inspect
+      puts "----------------------"
+      puts data.inspect
+      puts "======================"
+      nil
+    end
   end
 end
