cbbd02c22a4fa49e459401fb913459176e55d047
Tim Hemi (Ops) <tim@reinteractive.net>
2020-12-29 06:58:50 +1100

    add spec to make full coverage

--- spec/services/envisage_api_spec.rb
+++ spec/services/envisage_api_spec.rb
@@ -3,27 +3,42 @@ require 'rails_helper'
 RSpec.describe Envisage::Members do
 
   subject(:api) { described_class.new(org_token) }
+  let(:org_token) { "abc123xyx" }
 
   let(:response) { Net::HTTPSuccess.new(1.0, '200', 'OK') }
-  let(:body) { "{ \"records\": 20 }" }
 
   before {
     expect_any_instance_of(Net::HTTP).to receive(:request) { response }
     expect(response).to receive(:body) { body }
   }
 
-  let(:org_token) { "abc123xyx" }
-
-  it "get operates ok" do
-    api.get
-    expect(api.response_body).to include("records" => 20)
-    expect(api.error?).to be false
+  context "with method get" do
+    let(:body) { "{ \"records\": 20 }" }
+    it "get operates ok" do
+      api.get
+      expect(api.response_body).to include("records" => 20)
+      expect(api.error?).to be false
+    end
   end
 
-  it "post operates ok" do
-    api.post
-    expect(api.response_body).to include("records" => 20)
-    expect(api.error?).to be false
+  context "with method post" do
+    let(:body) { "{ \"thumbsup\": 3 }" }
+    it "post operates ok" do
+      api.post
+      expect(api.response_body).to include("thumbsup" => 3)
+      expect(api.error?).to be false
+    end
   end
+end
 
+# probably a bit perdantic but it gave me 100% coverage \o/
+RSpec.describe Envisage::Base do
+
+  class Dumby < Envisage::Base;end
+
+  context "with undefined `resource_path`" do
+    it "throws an error" do
+      expect{ Dumby.new.get }.to raise_error NotImplementedError
+    end
+  end
 end
