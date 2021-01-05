15b76fa9e0be32a8d1f80056adf753134d5074b4
Tim Hemi (Ops) <tim@reinteractive.net>
2021-01-06 07:35:26 +1100

    font issue startover use scss

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,6 @@ module Otcstats
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
