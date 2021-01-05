89ddba622f64e7439eff87e90973bbb866943ab9
Tim Hemi (Ops) <tim@reinteractive.net>
2021-01-06 07:26:04 +1100

    font issue undo yet again add config

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module Otcstats
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
