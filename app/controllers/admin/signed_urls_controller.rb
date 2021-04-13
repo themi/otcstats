9dddbabb3c23c6b0de86fe39f300a5ead7fbeec9
Tim Hemi (Ops) <tim@reinteractive.net>
2021-04-13 19:05:55 +1000

    adding vars

--- app/controllers/admin/signed_urls_controller.rb
+++ app/controllers/admin/signed_urls_controller.rb
@@ -21,7 +21,7 @@ module Admin
       attribs = {
         stream_id: @stream.short_url_key,
         short_path: SecureRandom.hex(10),
-        expires_in: 21600, # 6 hrs
+        expires_in: ENV.fetch(SIGNING_EXPIRES_IN).to_i,
       }
       @signed_url = SignedUrl.new(attribs)
     end
